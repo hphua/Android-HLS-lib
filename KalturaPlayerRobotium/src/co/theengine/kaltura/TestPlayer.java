87d18220c7ced7ac5577c85bec621b3e4ccf48d7
Miha Lunar <mlunar@gmail.com>
2014-08-15 18:23:36 -0400

    cleaned up, added comments

--- KalturaPlayerRobotium/src/co/theengine/kaltura/TestPlayer.java
+++ KalturaPlayerRobotium/src/co/theengine/kaltura/TestPlayer.java
@@ -1,27 +1,22 @@
 package co.theengine.kaltura;
 
-import java.io.File;
-import java.io.IOException;
-
-import junit.framework.TestCase;
-
-import com.robotium.solo.Solo;
-
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
-import android.test.InstrumentationTestCase;
+
+import com.robotium.solo.Solo;
 
 @SuppressWarnings("unchecked")
 public class TestPlayer extends ActivityInstrumentationTestCase2<Activity> {
 	private static final String LAUNCHER_ACTIVITY_FULL_CLASSNAME = "com.example.videoplayer.VideoPlayerActivity";
 	private static Class launcherActivityClass;
+	
+	/**
+	 * Retrieve the tested application class.
+	 */
 	static {
 		try {
-			System.out.println("WAY? " + LAUNCHER_ACTIVITY_FULL_CLASSNAME);
 			launcherActivityClass = Class.forName(LAUNCHER_ACTIVITY_FULL_CLASSNAME);
-			System.out.println("YAY");
 		} catch (ClassNotFoundException e) {
-			System.err.println("" + e);
 			throw new RuntimeException(e);
 		}
 	}
@@ -34,9 +29,13 @@ public class TestPlayer extends ActivityInstrumentationTestCase2<Activity> {
 	
 	/**
 	 * Incremental counter for screenshot files.
+	 * Used with the alternative
 	 */
-	private int screenshotCounter = 0;
+	//private int screenshotCounter = 0;
 
+	/**
+	 * Initialize the test.
+	 */
 	@Override
 	protected void setUp() throws Exception {
 		solo = new Solo(getInstrumentation(), getActivity());
@@ -46,6 +45,8 @@ public class TestPlayer extends ActivityInstrumentationTestCase2<Activity> {
 	 * Take a screenshot and save it with a sequential file name.
 	 */
 	private void screenshot() {
+		solo.takeScreenshot();
+		// Alternative way to screenshot (doesn't work for Robotium apparently)
 		/*
 		File f;
 		try {
@@ -58,31 +59,49 @@ public class TestPlayer extends ActivityInstrumentationTestCase2<Activity> {
 			// takeScreenshot doesn't seem to be that reliable across devices/emulators
 			//Boolean success = getUiDevice().takeScreenshot(f);
 			//assertTrue("Unable to take a screenshot", success);
+			
+			System.out.println("SCREENSHOTTING!!!! " + "screencap -p " + f.getAbsolutePath());
+			
 			Process process = Runtime.getRuntime().exec("screencap -p " + f.getAbsolutePath());
 			try {
+				System.out.println("Waiting...");
+
+				System.out.println("OUTPUT: "+IOUtils.toString(process.getInputStream()));
+				System.out.println("ERRORS: "+IOUtils.toString(process.getErrorStream()));
+				
 				process.waitFor();
+				
+				System.out.println("EXIT: "+process.exitValue());
+				
+				System.out.println("Done!");
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
+				System.out.println("Interrupted! " + e);
 				e.printStackTrace();
 			}
 		} catch (IOException e) {
+			System.out.println("IOException! " + e);
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		//*/
-		solo.takeScreenshot();
 	}
-
+	
+	/**
+	 * Sleep for the provided amount of milliseconds.
+	 */
 	private void sleep(int time) {
 		solo.sleep(time);
 	}
 	
+	/**
+	 * Test the provided stream menu option with screenshots and pause testing.
+	 * @param itemStream	Menu option name to test.
+	 */
 	private void streamTest(String itemStream) {
 		String itemPause = "Play/Pause";
 		
 		screenshot();
-		
-		sleep(2000);
 		
 		solo.clickOnMenuItem(itemStream);
 		
@@ -115,14 +134,23 @@ public class TestPlayer extends ActivityInstrumentationTestCase2<Activity> {
 		screenshot();
 	}
 	
+	/**
+	 * Test the ABC live stream via the menu option.
+	 */
 	public void testABC() {
 		streamTest("ABC DVR");
 	}
 	
+	/**
+	 * Test the Kaltura video on demand via the menu option.
+	 */
 	public void testKalturaVOD() {
 		streamTest("Kaltura VoD");
 	}
 	
+	/**
+	 * Finish the test.
+	 */
 	@Override
 	public void tearDown() throws Exception {
 		solo.finishOpenedActivities();
