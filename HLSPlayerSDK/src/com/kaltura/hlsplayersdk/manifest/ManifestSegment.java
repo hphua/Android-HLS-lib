5a3513b2e0c4ebb4974fd3f74b7cbb70f9debe16
Mark Fassett <mark@laughing-dragon.com>
2014-09-23 10:19:33 -0700

    Fixed AES can't seek backwards without crashing

--- HLSPlayerSDK/src/com/kaltura/hlsplayersdk/manifest/ManifestSegment.java
+++ HLSPlayerSDK/src/com/kaltura/hlsplayersdk/manifest/ManifestSegment.java
@@ -78,7 +78,7 @@ public class ManifestSegment extends BaseManifestItem
 		byte[] iv = hexStringToByteArray(ivStr);
 
 		cryptoId = SegmentCacheEntry.allocAESCryptoState(keyBytes.array(), iv);
-		Log.e("Crypto", "Got crypto ID " + cryptoId);
+		Log.i("Crypto", "Got crypto ID " + cryptoId);
 	}
 
 }
