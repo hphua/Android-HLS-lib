9554cccc9e6db359ad5998611f54b9179446247e
Ben Garney <ben.garney@gmail.com>
2014-10-12 21:19:39 -0700

    Cleaning up Makefile.

--- HLSPlayerSDK/jni/Android.mk
+++ HLSPlayerSDK/jni/Android.mk
@@ -4,11 +4,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := HLSPlayerSDK
 
-# libyuv
-#LOCAL_SRC_FILES += libyuv/video_common.cc libyuv/cpu_id.cc
-#LOCAL_SRC_FILES += libyuv/planar_functions.cc 
-#LOCAL_SRC_FILES += libyuv/row_common.cc libyuv/row_any.cc libyuv/row_neon.cc libyuv/row_posix.cc
-
 # Core Player Code
 LOCAL_SRC_FILES += HLSPlayerSDK.cpp HLSSegment.cpp HLSPlayer.cpp AudioTrack.cpp  RefCounted.cpp 
 LOCAL_SRC_FILES += androidVideoShim.cpp androidVideoShim_ColorConverter.cpp 
@@ -21,7 +16,7 @@ LOCAL_SRC_FILES += mpeg2ts_parser/AnotherPacketSource.cpp mpeg2ts_parser/AString
 LOCAL_SRC_FILES += mpeg2ts_parser/base64.cpp mpeg2ts_parser/ESQueue.cpp mpeg2ts_parser/hexdump.cpp mpeg2ts_parser/MPEG2TSExtractor.cpp 
 LOCAL_SRC_FILES += mpeg2ts_parser/SharedBuffer.cpp mpeg2ts_parser/VectorImpl.cpp
 
-LOCAL_CFLAGS += -DHAVE_SYS_UIO_H -Wno-multichar -Wno-pmf-conversions -O3
+LOCAL_CFLAGS += -DHAVE_SYS_UIO_H -Wno-multichar -Wno-pmf-conversions -g
 
 # -fdump-class-hierarchy
 LOCAL_C_INCLUDES += $(TOP)/system/core/include ./libyuv/
